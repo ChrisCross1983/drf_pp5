2679bbc75782b96c51861d7ad45dbeb139bc1b47
Christian Borza <christian.borza@students.codeinstitute.net>
2025-01-29 13:08:37 +0000

    Adjust serializers.py for frontend development

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,21 +3,37 @@ from .models import Post, Comment, SittingRequest, Notification
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Comment
-        fields = ['id', 'author', 'content', 'created_at']
+        fields = ['id', 'author', 'content', 'created_at', 'is_owner']
+
+    def get_is_owner(self, obj):
+        request = self.context.get("request")
+        return request and request.user == obj.author
+
 
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.username')
     likes_count = serializers.SerializerMethodField()
     comments = CommentSerializer(many=True, read_only=True)
+    is_owner = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'category', 'description', 'image', 'author', 'likes_count', 'comments', 'created_at']
+        fields = [
+            'id', 'title', 'category', 'description', 'image', 'author',
+            'likes_count', 'comments', 'created_at', 'is_owner'
+        ]
+
     def get_likes_count(self, obj):
         return obj.likes.count()
+
+    def get_is_owner(self, obj):
+        request = self.context.get("request")
+        return request and request.user == obj.author
+
 
 class SittingRequestSerializer(serializers.ModelSerializer):
     sender_username = serializers.ReadOnlyField(source='sender.username')
@@ -26,8 +42,12 @@ class SittingRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = SittingRequest
-        fields = ['id', 'sender', 'receiver', 'post', 'message', 'status', 'created_at', 'sender_username', 'receiver_username', 'post_title']
+        fields = [
+            'id', 'sender', 'receiver', 'post', 'message', 'status',
+            'created_at', 'sender_username', 'receiver_username', 'post_title'
+        ]
         read_only_fields = ['id', 'sender', 'receiver', 'post', 'created_at']
+
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
