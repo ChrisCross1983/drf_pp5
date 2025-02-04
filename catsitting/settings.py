cf525bec831db020fc3f59e048ec3b5c13f47653
Christian Borza <christian.borza@students.codeinstitute.net>
2025-02-04 13:01:48 +0000

    Fix missing path for sent requests

--- catsitting/settings.py
+++ catsitting/settings.py
@@ -88,7 +88,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,7 @@ from .views import (
     PostDetailView,
     CommentDetailView,
     CreateSittingRequestView,
+    SentSittingRequestsView,
     IncomingSittingRequestsView,
     ManageSittingRequestView,
     NotificationListView,
