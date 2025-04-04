2e2ece92a1a4bc13ae66c4c5d0b68564d46a58dc
Chris <christian.borza@students.codeinstitute.net>
2025-04-04 17:19:01 +0200

    Add updated_at to post / models.py

--- posts/models.py
+++ posts/models.py
@@ -30,7 +30,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-updated_at']
 
     def __str__(self):
         return self.title
