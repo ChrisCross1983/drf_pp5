f312cc7093c0055e1abf904486880ad75953997f
Chris <christian.borza@students.codeinstitute.net>
2025-04-07 12:22:27 +0200

    Fix: Loading error of comments, delete upadted_at / created_at serializers

--- comments/serializers.py
+++ comments/serializers.py
@@ -12,18 +12,6 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    created_at = serializers.DateTimeField(read_only=True)
-    updated_at = serializers.DateTimeField(read_only=True)
-
-    def get_is_owner(self, obj):
-        request = self.context['request']
-        return request.user == obj.owner
-
-    def get_created_at(self, obj):
-        return naturaltime(obj.created_at)
-
-    def get_updated_at(self, obj):
-        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Comment
@@ -32,6 +20,9 @@ class CommentSerializer(serializers.ModelSerializer):
             'post', 'created_at', 'updated_at', 'content'
         ]
 
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
 class CommentDetailSerializer(CommentSerializer):
     """
