6f374108044484e3196ff5dd67405d7725b2832d
Chris <christian.borza@students.codeinstitute.net>
2025-04-07 12:09:00 +0200

    Fix: Change time set up for comments in serializers.py back to DateTimeField

--- comments/serializers.py
+++ comments/serializers.py
@@ -12,8 +12,8 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    created_at = serializers.SerializerMethodField()
-    updated_at = serializers.SerializerMethodField()
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
 
     def get_is_owner(self, obj):
         request = self.context['request']
