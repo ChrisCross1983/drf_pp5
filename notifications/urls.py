622b4dd9901cd56429780d3349b664579db17b68
Chris <christian.borza@students.codeinstitute.net>
2025-04-08 17:35:45 +0200

    feat: Add dashboard overview API for recent notifications and comments

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import DashboardOverviewView
 from .views import NotificationListView, MarkNotificationReadView
 
 urlpatterns = [
     path('', NotificationListView.as_view(), name='notification-list'),
     path('<int:notification_id>/mark-read/', MarkNotificationReadView.as_view(), name='mark-notification-read'),
+    path('overview/', DashboardOverviewView.as_view(), name='dashboard-overview'),
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -4,7 +4,41 @@ from rest_framework import status
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from notifications.models import Notification
+from comments.models import Comment
+from comments.serializers import CommentSerializer
 from .serializers import NotificationSerializer
+from posts.models import Post
+
+
+class DashboardOverviewView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        # Unread notifications (max. 3)
+        notifications = Notification.objects.filter(user=request.user, is_read=False).order_by('-created_at')[:3]
+        notifications_data = NotificationSerializer(notifications, many=True).data
+
+        # Newest comments to own post (max. 3)
+        own_posts = Post.objects.filter(author=request.user)
+        comments = Comment.objects.filter(post__in=own_posts).order_by('-created_at')[:3]
+
+        # Additional fields on top
+        comments_data = [
+            {
+                "id": c.id,
+                "content": c.content,
+                "created_at": c.created_at,
+                "post_id": c.post.id,
+                "post_title": c.post.title
+            }
+            for c in comments
+        ]
+
+        return Response({
+            "notifications": notifications_data,
+            "comments": comments_data
+        })
+
 
 class NotificationListView(ListAPIView):
     """
