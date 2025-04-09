11e1fe01cd6f3bb72b1e822f9cc0b0f7c8a8d4c4
Chris <christian.borza@students.codeinstitute.net>
2025-04-09 13:12:12 +0200

    Add missing '' path to notifications / urls.py

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,6 +3,7 @@ from .views import DashboardOverviewView
 from .views import AllNotificationsView, MarkNotificationReadView
 
 urlpatterns = [
+    path('', AllNotificationsView.as_view(), name='notifications-feed'), 
     path('all/', AllNotificationsView.as_view(), name='all-notifications'),
     path('<int:notification_id>/mark-read/', MarkNotificationReadView.as_view(), name='mark-notification-read'),
     path('overview/', DashboardOverviewView.as_view(), name='dashboard-overview'),
