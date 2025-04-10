46fe4c2772c342b7470d6619905e29df83d985d0
Chris <christian.borza@students.codeinstitute.net>
2025-04-10 11:18:50 +0200

    Fix route for comment likes count

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from comments import views
-from .views import ToggleCommentLike
+from .views import CommentList, CommentDetail, ToggleCommentLike
 
 urlpatterns = [
-    path('', views.CommentList.as_view(), name="comments-list"),
-    path('comments/<int:pk>/like/', ToggleCommentLike.as_view(), name='comment-like'),
-    path('<int:pk>/', views.CommentDetail.as_view(), name="comments-detail")
+    path('', CommentList.as_view(), name="comments-list"),
+    path('<int:pk>/like/', ToggleCommentLike.as_view(), name='comment-like'),
+    path('<int:pk>/', CommentDetail.as_view(), name="comments-detail")
 ]
