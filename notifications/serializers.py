be113f60dfac277f69675a54a421eed0991750e9
Chris <christian.borza@students.codeinstitute.net>
2025-04-22 15:22:59 +0200

    Add sender_profile_id to NotificationSerializer for follow request redirection

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -5,14 +5,20 @@ from notifications.models import Notification
 class NotificationSerializer(serializers.ModelSerializer):
     post_id = serializers.SerializerMethodField()
     sitting_request_id = serializers.SerializerMethodField()
+    sender_profile_id = serializers.SerializerMethodField()
 
     class Meta:
         model = Notification
-        fields = ['id', 'message', 'is_read', 'created_at', 'type',
-                  'post_id', 'sitting_request_id']
+        fields = [
+            'id', 'message', 'is_read', 'created_at', 'type',
+            'post_id', 'sitting_request_id', 'sender_profile_id'
+        ]
 
     def get_post_id(self, obj):
         return obj.post.id if obj.post else None
 
     def get_sitting_request_id(self, obj):
         return obj.sitting_request.id if obj.sitting_request else None
+    
+    def get_sender_profile_id(self, obj):
+        return obj.sender.profile.id if obj.sender and hasattr(obj.sender, "profile") else None
