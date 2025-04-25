970462d1b972e700328acf57379d3ce2ecc1cc2c
Chris <christian.borza@students.codeinstitute.net>
2025-04-25 18:21:49 +0200

    Add notification functionality to chat messages in sitting requests

--- posts/views_messages.py
+++ posts/views_messages.py
@@ -3,6 +3,7 @@ from .models import SittingResponseMessage
 from .serializers import SittingResponseMessageSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
+from notifications.models import Notification
 
 
 class SittingResponseMessageViewSet(viewsets.ModelViewSet):
@@ -14,11 +15,20 @@ class SittingResponseMessageViewSet(viewsets.ModelViewSet):
         sitting_request = serializer.validated_data["sitting_request"]
         content = serializer.validated_data["content"]
 
-        serializer.save(
+        message = serializer.save(
             sender=self.request.user,
             sitting_request=sitting_request,
             content=content
         )
+
+        Notification.objects.create(
+            user=sitting_request.receiver if self.request.user == sitting_request.sender else sitting_request.sender,
+            type="sitting_message",
+            sitting_request=sitting_request,
+            message=f"New message on your sitting request for post '{sitting_request.post.title}'"
+        )
+
+        return message
 
     def get_queryset(self):
         """
